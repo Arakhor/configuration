2c4c93d2cfe95245382bff293f97a680f6878fb1
arakhor <arakhor@pm.me>
2025-12-22 02:43:13 +0100

    fix zephFinal

--- core/disks/zeph.mod.nix
+++ core/disks/zeph.mod.nix
@@ -1,172 +1,130 @@
 let
-  username = "arakhor";
   hostname = "zeph";
-  poolName = "${hostname}-zpool";
-  pseudoRoot = "${hostname}-nixos";
 in
 {
-  zeph.disko.devices = {
-    disk = {
-      "2TB-NVME-0" = {
+  zeph.disko.devices =
+    { lib, ... }:
+    {
+      disk = lib.genAttrs [ "0" "1" ] (i: {
         type = "disk";
-        device = "/dev/nvme0n1";
+        device = "/dev/nvme${i}";
         content = {
           type = "gpt";
           partitions = {
-            esp = {
-              name = "boot";
-              size = "1024M";
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
+            ESP = {
+              size = "1G";
               type = "EF00";
               content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [
-                  "defaults"
-                  "umask=0077"
-                ];
+                type = "mdraid";
+                name = "boot";
               };
             };
-            zfs = {
+            mdadm = {
               size = "100%";
               content = {
-                type = "zfs";
-                pool = poolName;
+                type = "mdraid";
+                name = "raid1";
               };
+            };
+          };
+        };
+      });
+
+      mdadm = {
+        boot = {
+          type = "mdadm";
+          level = 1;
+          metadata = "1.0";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "umask=0077" ];
+          };
+        };
+        raid1 = {
+          type = "mdadm";
+          level = 1;
+          content = {
+            type = "luks";
+            name = "${hostname}-crypted";
+
+            askPassword = true;
+            settings = {
+              allowDiscards = true; # enables trimming support
+              bypassWorkqueues = true; # improves performance
+              crypttabExtraOpts = [
+                "tries=5"
+                # We don't need to measure the volume key in a PCR but this
+                # is needed to allow multiple tries of TPM pins
+                # https://github.com/systemd/systemd/issues/32041
+                "tpm2-measure-pcr=yes"
+                # I think the above disables the cryptsetup token plugin (the prompt that
+                # starts with "Please enter LUKS2 token PIN: ") so we've got to use native
+                # systemd TPM pin unlock
+                "tpm2-pin=yes"
+                "tpm2-device=auto"
+              ];
+            };
+
+            # https://www.man7.org/linux/man-pages/man8/cryptsetup-luksFormat.8.html
+            extraFormatArgs = [
+              "--type=luks2"
+              "--use-random" # true randomness at the cost of blocking if there isn't enough entropy
+            ];
+
+            content = {
+              type = "lvm_pv";
+              vg = "${hostname}-nixos";
             };
           };
         };
       };
 
-      "2TB-NVME-1" = {
-        type = "disk";
-        device = "/dev/nvme1n1";
-        content = {
-          type = "gpt";
-          partitions.zfs = {
+      lvm_vg."${hostname}-nixos" = {
+        type = "lvm_vg";
+        lvs = {
+          swap = {
+            size = "96G";
+            content = {
+              type = "swap";
+              resumeDevice = true;
+            };
+          };
+
+          nix = {
+            size = "200G";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/nix";
+              mountOptions = [ "noatime" ];
+            };
+          };
+
+          state = {
             size = "100%";
             content = {
-              type = "zfs";
-              pool = "${poolName}-1";
-            };
-          };
-        };
-      };
-    };
-
-    nodev."/" = {
-      fsType = "tmpfs";
-      mountOptions = [
-        "defaults"
-        "mode=755"
-      ];
-    };
-
-    zpool =
-      let
-        rootFsOptions = {
-          atime = "off";
-          mountpoint = "none";
-          xattr = "sa";
-          acltype = "posixacl";
-          compression = "lz4";
-        };
-      in
-      {
-        ${poolName} = {
-          type = "zpool";
-          options.ashift = "12";
-          inherit rootFsOptions;
-
-          datasets = {
-            ${pseudoRoot}.type = "zfs_fs";
-
-            "${pseudoRoot}/nix" = {
-              type = "zfs_fs";
-              mountpoint = "/nix";
-              options.mountpoint = "legacy";
-            };
-
-            "${pseudoRoot}/state" = {
-              type = "zfs_fs";
-              options = {
-                encryption = "aes-256-gcm";
-                keyformat = "passphrase";
-                keylocation = "prompt";
-              };
-            };
-
-            "${pseudoRoot}/state/steam" = {
-              type = "zfs_fs";
-              mountpoint = "/state/home/${username}/.local/share/Steam";
-              options.mountpoint = "legacy";
-              options.recordsize = "1M";
-            };
-
-            "${pseudoRoot}/state/games" = {
-              type = "zfs_fs";
-              mountpoint = "/state/home/${username}/games";
-              options.mountpoint = "legacy";
-              options.recordsize = "1M";
-            };
-
-            "${pseudoRoot}/state/models" = {
-              type = "zfs_fs";
-              mountpoint = "/state/var/lib/private/ollama";
-              options.mountpoint = "legacy";
-              options.recordsize = "1M";
-            };
-          };
-        };
-
-        "${poolName}-1" = {
-          type = "zpool";
-          options.ashift = "12";
-          inherit rootFsOptions;
-
-          datasets = {
-            ${pseudoRoot}.type = "zfs_fs";
-
-            "${pseudoRoot}/state" = {
-              type = "zfs_fs";
+              type = "filesystem";
+              format = "ext4";
               mountpoint = "/state";
-              options = {
-                mountpoint = "legacy";
-                encryption = "aes-256-gcm";
-                keyformat = "passphrase";
-                keylocation = "prompt";
-              };
-            };
-
-            "${pseudoRoot}/state/videos" = {
-              type = "zfs_fs";
-              mountpoint = "/state/home/${username}/videos";
-              options.mountpoint = "legacy";
-              options.recordsize = "1M";
-            };
-
-            "${pseudoRoot}/state/pictures" = {
-              type = "zfs_fs";
-              mountpoint = "/state/home/${username}/pictures";
-              options.mountpoint = "legacy";
-              options.recordsize = "1M";
-            };
-
-            "${pseudoRoot}/state/music" = {
-              type = "zfs_fs";
-              mountpoint = "/state/home/${username}/music";
-              options.mountpoint = "legacy";
-              options.recordsize = "1M";
-            };
-
-            "${pseudoRoot}/state/downloads" = {
-              type = "zfs_fs";
-              mountpoint = "/state/home/${username}/downloads";
-              options.mountpoint = "legacy";
-              options.recordsize = "1M";
+              mountOptions = [ "noatime" ];
             };
           };
         };
       };
-  };
+
+      nodev."/" = {
+        fsType = "tmpfs";
+        mountOptions = [
+          "defaults"
+          "mode=755"
+          "size=32g"
+        ];
+      };
+    };
 }
