f765413a1560e0bb7c316ed1e5d834d19085f184
arakhor <arakhor@pm.me>
2025-12-22 04:47:18 +0100

    fixes

--- core/disks/zeph.mod.nix
+++ core/disks/zeph.mod.nix
@@ -16,7 +16,7 @@
               type = "EF00";
               content = {
                 type = "mdraid";
-                name = "boot";
+                name = "esp";
               };
             };
 
@@ -24,7 +24,7 @@
               size = "100%";
               content = {
                 type = "mdraid";
-                name = "raid1";
+                name = "luks";
               };
             };
           };
@@ -57,22 +57,15 @@
               bypassWorkqueues = true;
               crypttabExtraOpts = [
                 "tries=5"
-                # We don't need to measure the volume key in a PCR but this
-                # is needed to allow multiple tries of TPM pins
-                # https://github.com/systemd/systemd/issues/32041
                 "tpm2-measure-pcr=yes"
-                # I think the above disables the cryptsetup token plugin (the prompt that
-                # starts with "Please enter LUKS2 token PIN: ") so we've got to use native
-                # systemd TPM pin unlock
                 "tpm2-pin=yes"
                 "tpm2-device=auto"
               ];
             };
 
-            # https://www.man7.org/linux/man-pages/man8/cryptsetup-luksFormat.8.html
             extraFormatArgs = [
               "--type=luks2"
-              "--use-random" # true randomness at the cost of blocking if there isn't enough entropy
+              "--use-random"
             ];
 
             content = {
