a86c206e80d44c655a9238e708d100307da891c0
arakhor <arakhor@pm.me>
2025-12-22 03:42:44 +0100

    fixes

--- core/disks/zeph.mod.nix
+++ core/disks/zeph.mod.nix
@@ -1,9 +1,9 @@
-let
-  hostname = "zeph";
-in
 {
   zeph.disko.devices =
     { lib, ... }:
+    let
+      hostname = "zeph";
+    in
     {
       disk = lib.genAttrs [ "0" "1" ] (i: {
         type = "disk";
@@ -11,108 +11,72 @@ in
         content = {
           type = "gpt";
           partitions = {
-            boot = {
-              size = "1M";
-              type = "EF02"; # for grub MBR
-            };
-            ESP = {
+            ESP = lib.mkIf (i == "0") {
               size = "1G";
               type = "EF00";
               content = {
-                type = "mdraid";
-                name = "boot";
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
               };
             };
-            mdadm = {
+
+            raid = {
               size = "100%";
               content = {
                 type = "mdraid";
-                name = "raid1";
+                name = "root";
               };
             };
           };
         };
       });
 
-      mdadm = {
-        boot = {
-          type = "mdadm";
-          level = 1;
-          metadata = "1.0";
+      mdadm.root = {
+        type = "mdadm";
+        level = 1;
+        metadata = "1.2";
+        content = {
+          type = "luks";
+          name = "${hostname}-crypt";
+          askPassword = true;
+          settings = {
+            allowDiscards = true;
+            bypassWorkqueues = true;
+          };
+
           content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
-            mountOptions = [ "umask=0077" ];
-          };
-        };
-        raid1 = {
-          type = "mdadm";
-          level = 1;
-          content = {
-            type = "luks";
-            name = "${hostname}-crypted";
+            type = "lvm_vg";
+            name = "${hostname}-vg";
+            lvs = {
+              swap = {
+                size = "96G";
+                content = {
+                  type = "swap";
+                  resumeDevice = true;
+                };
+              };
 
-            askPassword = true;
-            settings = {
-              allowDiscards = true; # enables trimming support
-              bypassWorkqueues = true; # improves performance
-              crypttabExtraOpts = [
-                "tries=5"
-                # We don't need to measure the volume key in a PCR but this
-                # is needed to allow multiple tries of TPM pins
-                # https://github.com/systemd/systemd/issues/32041
-                "tpm2-measure-pcr=yes"
-                # I think the above disables the cryptsetup token plugin (the prompt that
-                # starts with "Please enter LUKS2 token PIN: ") so we've got to use native
-                # systemd TPM pin unlock
-                "tpm2-pin=yes"
-                "tpm2-device=auto"
-              ];
-            };
+              nix = {
+                size = "200G";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/nix";
+                  mountOptions = [ "noatime" ];
+                };
+              };
 
-            # https://www.man7.org/linux/man-pages/man8/cryptsetup-luksFormat.8.html
-            extraFormatArgs = [
-              "--type=luks2"
-              "--use-random" # true randomness at the cost of blocking if there isn't enough entropy
-            ];
-
-            content = {
-              type = "lvm_pv";
-              vg = "${hostname}-nixos";
-            };
-          };
-        };
-      };
-
-      lvm_vg."${hostname}-nixos" = {
-        type = "lvm_vg";
-        lvs = {
-          swap = {
-            size = "96G";
-            content = {
-              type = "swap";
-              resumeDevice = true;
-            };
-          };
-
-          nix = {
-            size = "200G";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/nix";
-              mountOptions = [ "noatime" ];
-            };
-          };
-
-          state = {
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/state";
-              mountOptions = [ "noatime" ];
+              state = {
+                size = "100%";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/state";
+                  mountOptions = [ "noatime" ];
+                };
+              };
             };
           };
         };
@@ -121,9 +85,8 @@ in
       nodev."/" = {
         fsType = "tmpfs";
         mountOptions = [
-          "defaults"
           "mode=755"
-          "size=32g"
+          "size=48g"
         ];
       };
     };
